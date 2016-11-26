3f11db292ffd6affa3b4978b2b9e9372a63606a9
quadratus <amiteshwarsidhu7@gmail.com>
2016-11-26 19:09:18 +0530

    Main

--- app/src/main/java/com/team_f/hercules/exer.java
+++ app/src/main/java/com/team_f/hercules/exer.java
@@ -9,6 +9,6 @@ public class exer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exer);
-        //Main
+        //Main test
     }
 }
